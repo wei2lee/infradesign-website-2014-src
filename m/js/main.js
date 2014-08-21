0d253cddb216123e8401957164102504b45963dc
lee yee chuan <wei2lee86@gmail.com>
2014-08-21 15:11:38 +0800

    progress

--- m/js/main.js
+++ m/js/main.js
@@ -51,6 +51,12 @@ Controller.prototype = {
     init : function() {
         this.$sel = $(this.sel);   
     },
+    onPostInit : function() {
+        
+    },
+    onInitialized : function() {
+        
+    },
     show : function(a) {
         if(this.$sel.is(':visible'))return;
         $(window).on('resize', this, this._onResize);
@@ -138,7 +144,8 @@ SliderController.prototype = $.extend(Object.create(Controller.prototype), {
         this.$slider.goToSlide(0);   
     },
     onKeyDown : function(evt){
-        
+        if(evt.keyCode == 37) this.$slider.goToPrevSlide()
+        else if(evt.keyCode == 39) this.$slider.goToNextSlide()
     },
     onResize : function(evt){
         var ww = $(window).width();
@@ -180,6 +187,7 @@ PlatformController.prototype = $.extend(Object.create(SliderController.prototype
         });
     }
 });
+
 
 function CrewController() {
     this.sel = '#crew';
@@ -250,9 +258,261 @@ function FoliosController() {
     return this;   
 }
 FoliosController.prototype = $.extend(Object.create(SliderController.prototype), {
+    preload : {
+        state : 0,
+        loaded : 0,
+        total : 0,
+        objs : [],
+        startAnimOnPreloaded : false
+    },
+    foliosanim1delay : null,
+    foliosanim1 : null,
+    foliosanim1_1 : null,
+    foliosanim2delay : null,
+    foliosanim2 : null,
+    isinitcycle : true,
+    
     init : function() {
         SliderController.prototype.init.call(this);
+        this.preload.startAnimOnPreloaded = false;
+        this.preloadFoliosImage();
     },
+    stopPreloadFoliosImage : function() {
+        var _this = this;
+        if(this.preload.state == 1){
+            this.preload.state = 0;
+            this.preload.loaded = 0;
+            this.preload.total = 0;
+            $.each(this.preload.objs, function(i,obj){
+                obj.img.onload = null;
+            });
+            this.preload.objs = [];
+        }
+    },
+    preloadFoliosImage : function() { console.log('preloadFoliosImage');
+        
+        this.preload.state = 1;
+        this.preload.total = this.$sel.find(".folios_page>div").length;
+        this.preload.loaded = 0;
+        this.preload.objs = []; 
+        
+        var _this = this;
+        
+        this.$sel.find(".folios_page>div").each(function(i, ele) {
+            var backgroundimage = $(this).css('background-image');
+            var reg = /url\((.+?)\)/;
+            var m = reg.exec(backgroundimage);
+            var obj = { 'ele':$(this), 'img': new Image() };
+            obj.img.onload = function() { 
+                _this.preload.loaded++;
+                console.log('preloadFoliosImage@img.onload, loaded='+_this.preload.loaded + ',' + _this.preload.total);
+                if (_this.preload.loaded == _this.preload.total && _this.preload.state == 1) {
+                    _this.preload.state = 2;
+                    _this.onPreloadedFoliosImage();
+                }
+                obj.img.onload = null;
+            }
+            obj.img.src = m[1];
+            _this.preload.objs.push(obj);
+        });
+    },
+
+    onPreloadedFoliosImage : function() { console.log('onPreloadedFoliosImage');
+        var _this = this;
+        $.each(this.preload.objs, function(i, obj) {
+            var grayScaleData = Filters.filterImage(Filters.grayscale, obj.img);
+            var grayScaleDataUrl = grayScaleData.canvas.toDataURL("image/png")
+            var style = 
+                "left:0px; top:0px; " + 
+                "width:" + obj.ele.css('width') + ";" + 
+                "height:" + obj.ele.css('height') + ";" +
+                "background-image:url(" + grayScaleDataUrl + ")";
+            obj.ele.empty();
+            obj.ele.append($("<div class='grayscale' style='" + style + "';></div>"));
+        });
+        if(this.preload.startAnimOnPreloaded) this.startFoliosAnimation();
+    },
+    
+    stopFoliosAnimation : function() { console.log('stopFoliosAnimation');
+        if(this.foliosanim1delay){
+            this.foliosanim1delay.kill();
+            this.foliosanim1.kill();
+            this.foliosanim1_1.kill();
+            this.foliosanim2delay.kill();
+            this.foliosanim2.kill();
+        }
+        TweenMax.set($("#folios_page1>div"), { alpha: 0 });
+        TweenMax.set($("#folios_page2>div"), { alpha: 0 });
+        $("#folios_page1").css("pointer-events", "none");
+        $("#folios_page2").css("pointer-events", "none");
+        $("#folios_page1>div").unbind('mouseenter mouseleave mouseover mouseout');
+        $("#folios_page1").unbind('mouseenter mouseleave mouseover mouseout');
+        $("#folios_page2>div").unbind('mouseenter mouseleave mouseover mouseout');
+        $("#folios_page2").unbind('mouseenter mouseleave mouseover mouseout');
+    },
+    
+    startFoliosAnimation : function() { console.log('startFoliosAnimation');
+        var _this = this;
+        var pagedelay = 2;
+        var pagedelay2 = 1.5;
+        var alphatweendelay = 0.1;
+        var randomdelayk = 0.3;
+        var tweenduration = 0.7;
+        var leftdelayk = 0.0040;
+        var pagewidth = 320;
+
+        this.isinitcycle = true;
+        this.foliosanim1delay = new TimelineMax({paused: true, overwrite:'none'});
+        this.foliosanim1 = new TimelineMax({paused: true, overwrite:'none'});
+        this.foliosanim1_1 = new TimelineMax({paused: true, overwrite:'none'});
+        this.foliosanim2delay = new TimelineMax({paused: true, overwrite:'none'});
+        this.foliosanim2 = new TimelineMax({paused: true, overwrite:'none'});
+
+        TweenMax.set($(".folios_page"), { perspective: 2000 });
+        TweenMax.set($("#folios_page1>div"), { alpha: 0 });
+        TweenMax.set($("#folios_page2>div"), { alpha: 0 });
+        $("#folios_page1").css("pointer-events", "none");
+        $("#folios_page2").css("pointer-events", "none");
+        $("#folios_page1>div").unbind('mouseenter mouseleave mouseover mouseout');
+        $("#folios_page1").unbind('mouseenter mouseleave mouseover mouseout');
+        $("#folios_page2>div").unbind('mouseenter mouseleave mouseover mouseout');
+        $("#folios_page2").unbind('mouseenter mouseleave mouseover mouseout');
+                                       
+        this.foliosanim1delay.insert(TweenMax.to({x: 0}, pagedelay, {x: 0}));
+                                       
+        this.foliosanim1delay.append(TweenMax.delayedCall(0, function() {
+            console.log("FoliosAnimation-1");
+            _this.foliosanim1.restart();
+        }));
+
+        this.foliosanim1.insert(TweenMax.delayedCall(0, function() {
+            console.log("FoliosAnimation-2-isinitcycle="+_this.isinitcycle);
+            $("#folios_page1").show();
+            $("#folios_page1").css("pointer-events", "visible");
+            $("#folios_page2").css("pointer-events", "none");
+            if (_this.isinitcycle) {
+                TweenMax.set($("#folios_page2>div"), { alpha: 0 });
+                TweenMax.set($("#folios_page1>div"), { alpha: 0 });
+            }
+            $("#folios_page2>div").unbind('mouseenter mouseleave mouseover mouseout');
+            $("#folios_page2").unbind('mouseenter mouseleave mouseover mouseout');
+        }));
+        $("#folios_page2>div").each(function() {
+            var left = parseInt($(this).css('left'));
+            var randomdelay = Math.random() * randomdelayk;
+            _this.foliosanim1.insert(TweenMax.fromTo($(this), tweenduration, 
+            { alpha: 1 }, { alpha: 0 }), 
+            (pagewidth - left) * leftdelayk + randomdelay + alphatweendelay);
+            
+            _this.foliosanim1.insert(TweenMax.fromTo($(this), tweenduration, 
+            { rotationY: 0 }, { rotationY: -180, transformOrigin: "center center" }), 
+            (pagewidth - left) * leftdelayk + randomdelay);
+        });
+//return;
+        $("#folios_page1>div").each(function() {
+            var left = parseInt($(this).css('left'));
+            var randomdelay = Math.random() * randomdelayk;
+            _this.foliosanim1_1.insert(TweenMax.fromTo($(this), tweenduration, 
+            { alpha: 0 }, { alpha: 1, ease: Quad.easeIn }), 
+            0 + 0.5 + left * leftdelayk + randomdelay + alphatweendelay);
+            _this.foliosanim1_1.insert(TweenMax.fromTo($(this), tweenduration, 
+            { rotationY: -200 }, { rotationY: 0, transformOrigin: "center center" }), 
+            0 + 0.5 + left * leftdelayk + randomdelay);
+        });
+        this.foliosanim1.insert(TweenMax.delayedCall(pagedelay2, function(){ _this.foliosanim1_1.restart(); }));
+        this.foliosanim1_1.append(TweenMax.delayedCall(0, function() {
+            console.log("FoliosAnimation-3");
+            _this.foliosanim2delay.restart();
+            $("#folios_page1>div").hover(function() {
+                _this.foliosanim2delay.pause();
+            });
+            $("#folios_page1").hover(
+                function() {
+                    _this.foliosanim2delay.pause();
+                },
+                function() {
+                    _this.foliosanim2delay.resume();
+                }
+            );
+        }));
+//return;
+        this.foliosanim2delay.insert(TweenMax.to({ x: 0 }, pagedelay, { x: 0 }));
+        this.foliosanim2delay.append(TweenMax.delayedCall(0, function() {
+            console.log("FoliosAnimation-4");
+            _this.foliosanim2.restart();
+        }));
+//return;
+        this.foliosanim2.insert(TweenMax.delayedCall(0, function() {
+            console.log("FoliosAnimation-5");
+            $("#folios_page2").show();
+            $("#folios_page1").css("pointer-events", "none");
+            $("#folios_page2").css("pointer-events", "visible");
+            $("#folios_page1>div").unbind('mouseenter mouseleave mouseover mouseout');
+            $("#folios_page1").unbind('mouseenter mouseleave mouseover mouseout');
+        }));
+//return;
+        $("#folios_page1>div").each(function() {
+            var left = parseInt($(this).css('left'));
+            var randomdelay = Math.random() * randomdelayk;
+            _this.foliosanim2.insert(TweenMax.fromTo($(this), tweenduration, 
+            { alpha: 1 }, { alpha: 0 }), 
+            left * leftdelayk + randomdelay + alphatweendelay);
+            _this.foliosanim2.insert(TweenMax.fromTo($(this), tweenduration, 
+            { rotationY: 0 }, { rotationY: 180, transformOrigin: "center center" }), 
+            left * leftdelayk + randomdelay);
+        });
+//return;
+        $("#folios_page2>div").each(function() {
+            var left = parseInt($(this).css('left'));
+            var randomdelay = Math.random() * randomdelayk;
+            _this.foliosanim2.insert(TweenMax.fromTo($(this), tweenduration, 
+            { alpha: 0 }, { alpha: 1, ease: Quad.easeIn }), 
+            pagedelay2 + (pagewidth - left) * leftdelayk + randomdelay + alphatweendelay);
+            _this.foliosanim2.insert(TweenMax.fromTo($(this), tweenduration, 
+            { rotationY: 200 }, { rotationY: 0, transformOrigin: "center center" }), 
+            pagedelay2 + (pagewidth - left) * leftdelayk + randomdelay);
+        });
+
+        this.foliosanim2.append(TweenMax.delayedCall(0, function() {
+            console.log("FoliosAnimation-6");
+            _this.isinitcycle = false;
+            _this.foliosanim1delay.restart();
+            $("#folios_page2>div").hover(function() {
+                _this.foliosanim1delay.pause();
+            });
+            $("#folios_page2").hover(
+                function() {
+                    _this.foliosanim1delay.pause();
+                },
+                function() {
+                    _this.foliosanim1delay.resume();
+                }
+            );
+        }));
+
+        TweenMax.set($("#folios_page1>div"), { alpha: 0 });
+        TweenMax.set($("#folios_page2>div"), { alpha: 0 });
+        this.foliosanim1_1.restart();
+    },
+    
+    onShown : function(a) {         console.log('onShown');
+        SliderController.prototype.onShown.call(this,a);
+        this.stopPreloadFoliosImage();  
+        this.stopFoliosAnimation();
+        if(this.preload.state == 0){
+            this.preload.startAnimOnPreloaded = true;
+            this.preloadFoliosImage();
+        }else if(this.preload.state == 2){
+            this.startFoliosAnimation();
+        }
+    },
+    
+    onHide : function(a) {
+        SliderController.prototype.onHide.call(this,a);
+        this.stopPreloadFoliosImage();  
+        this.stopFoliosAnimation();
+    },
+    
     onResize : function(evt) {
         SliderController.prototype.onResize.call(this);
         this.$sel.find('[data-scale-fit-width]').each(function(ind, ele) {
